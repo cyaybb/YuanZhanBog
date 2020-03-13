7d2eaf6bc1e7a9a183bc2309e68de65e2fb2f98c
402290540@qq.com <402290540@qq.com>
2020-03-13 16:58:19 +0800

    完成register的页面view展示

--- UI/Controllers/RegisterController.cs
+++ UI/Controllers/RegisterController.cs
@@ -2,10 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace UI.Controllers
 {
-    public class RegisterController
+    public class RegisterController:Controller
     {
+        public ActionResult New()
+        {
+            return View();
+        }
     }
 }
