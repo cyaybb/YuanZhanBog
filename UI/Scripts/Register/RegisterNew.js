81f8b7bcad7282c9ea4bd64d4d8fc05cfb675000
402290540@qq.com <402290540@qq.com>
2020-03-14 19:54:38 +0800

    完成注册时的验证码的显示功能

---
+++ UI/Scripts/Register/RegisterNew.js
@@ -0,0 +1,10 @@
+﻿
+document.getElementsByClassName('cy-click-validateCode')[0]
+    .onclick = function show() {
+        document.getElementsByClassName('cy-click-contentHidden')[0]
+            .style.display = 'none';
+        document.getElementsByClassName('cy-click-contentShow')[0]
+            .style.visibility = '';
+    }
+
+    
