fa3888cc7c9b0f0bce52d5a2b600e111df008df3
402290540@qq.com <402290540@qq.com>
2020-03-14 21:10:39 +0800

    新建RegisterNewModel并添加相应的属性

---
+++ SRV/ViewModel/Register/NewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewModel.Register
+{
+    public class NewModel
+    {
+        public string Inviter { get; set; }
+        public string InviterCode { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+        public string VerificationCode { get; set; }
+    }
+}
