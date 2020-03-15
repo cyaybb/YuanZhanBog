941bacbcdda4dc68cdee4132ff8326f03ee8a1ff
402290540@qq.com <402290540@qq.com>
2020-03-15 11:22:24 +0800

    完成registerNew view的model binding

--- UI/Controllers/RegisterController.cs
+++ UI/Controllers/RegisterController.cs
@@ -3,12 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ViewModel.Register;
 
 namespace UI.Controllers
 {
     public class RegisterController:Controller
     {
+        [HttpGet]
         public ActionResult New()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult New(NewModel newModel)
         {
             return View();
         }
