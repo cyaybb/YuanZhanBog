086242b3b8397c0a3dcbf49ef655051cc170c6b2
402290540@qq.com <402290540@qq.com>
2020-03-19 19:32:53 +0800

    UserRepository继承DBContext 和添加属性DBset<User>

--- BLL/Repository/UserRepository.cs
+++ BLL/Repository/UserRepository.cs
@@ -1,13 +1,20 @@
-﻿using System;
+﻿using Entity;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Repository
 {
-    public class UserRepository
+    public class UserRepository : DbContext
     {
+        public DbSet<User> Users { get; set; }
 
+        public User Save()
+        {
+            return new User();
+        }
     }
 }
