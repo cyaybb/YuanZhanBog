622acdf304daaca31c398da797c4acb5fc9073ab
402290540@qq.com <402290540@qq.com>
2020-03-19 19:19:33 +0800

    构建Entity User类

---
+++ BLL/Entity/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public int InviterId { get; set; }
+        public int InviteCode { get; set; }
+    }
+}
