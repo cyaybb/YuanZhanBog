8924dc68456313ec74958ed37b19d1e66de7b4f1
402290540@qq.com <402290540@qq.com>
2020-03-19 18:35:07 +0800

    取消注释

--- SRV/ViewModel/Register/NewModel.cs
+++ SRV/ViewModel/Register/NewModel.cs
@@ -11,30 +11,30 @@ namespace ViewModel.Register
 {
     public class NewModel
     {
-        //[Required(ErrorMessage ="*邀请人不能为空")]
+        [Required(ErrorMessage = "*邀请人不能为空")]
         public string Inviter { get; set; }
 
-        //[Required(ErrorMessage = "*邀请码不能为空")]
-        //[RegularExpression("[0-9]{4}", ErrorMessage = "*邀请码只能是4位数字")]
+        [Required(ErrorMessage = "*邀请码不能为空")]
+        [RegularExpression("[0-9]{4}", ErrorMessage = "*邀请码只能是4位数字")]
         public string InviterCode { get; set; }
 
-        //[Required(ErrorMessage = "*用户名不能为空")]
-        //[CheckNameRepeat(ErrorMessage ="*用户名重复")]//-----------
-        //[Remote("CheckNameRepeat","Register",ErrorMessage = "*用户名重复",HttpMethod ="Get")]
+        [Required(ErrorMessage = "*用户名不能为空")]
+        [CheckNameRepeat(ErrorMessage = "*用户名重复")]//-----------
+        [Remote("CheckNameRepeat", "Register", ErrorMessage = "*用户名重复", HttpMethod = "Get")]
         public string UserName { get; set; }
 
-        //[MinLength(4,ErrorMessage = "*密码的长度不能小于4，大于20")]
-        //[MaxLength(20, ErrorMessage = "*密码的长度不能小于4，大于20")]
-        //[Required(ErrorMessage = "*密码不能为空")]
+        [MinLength(4, ErrorMessage = "*密码的长度不能小于4，大于20")]
+        [MaxLength(20, ErrorMessage = "*密码的长度不能小于4，大于20")]
+        [Required(ErrorMessage = "*密码不能为空")]
         public string Password { get; set; }
 
-        //[Compare("Password",ErrorMessage = "*确认密码和密码不一致")]
-        //[Required(ErrorMessage = "*确认密码不能为空")]
+        [System.ComponentModel.DataAnnotations.Compare("Password", ErrorMessage = "*确认密码和密码不一致")]
+        [Required(ErrorMessage = "*确认密码不能为空")]
         public string ConfirmPassword { get; set; }
 
-        //[Required(ErrorMessage = "*验证码不能为空")]
-        //[StringLength(4, ErrorMessage = "*验证码的长度只能等于4",MinimumLength =4)]
-        //[CheckValidateCode(ErrorMessage ="*验证码错误")]
+        [Required(ErrorMessage = "*验证码不能为空")]
+        [StringLength(4, ErrorMessage = "*验证码的长度只能等于4", MinimumLength = 4)]
+        [CheckValidateCode(ErrorMessage = "*验证码错误")]
         [Remote("AfterEndValidationCode", "Register",ErrorMessage ="*验证码错误",HttpMethod ="Get")]
         public string VerificationCode { get; set; }
     }
