2d3aef5b4380bf71592e00077ca4e702872cdf3a
402290540@qq.com <402290540@qq.com>
2020-03-20 19:22:14 +0800

    nothing

---
+++ BLL/Repository/Migrations/202003200939399_UserInviteCodeAddUnique.cs
@@ -0,0 +1,18 @@
+﻿namespace Repository.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class UserInviteCodeAddUnique : DbMigration
+    {
+        public override void Up()
+        {
+            CreateIndex("dbo.Users", "InviteCode", unique: true, name: "IX_Users_Invitecode");
+        }
+        
+        public override void Down()
+        {
+            DropIndex("dbo.Users", "IX_Users_Invitecode");
+        }
+    }
+}
