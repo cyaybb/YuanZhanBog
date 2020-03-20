b10f007f1744028965b08afe97a136018f8a9c18
402290540@qq.com <402290540@qq.com>
2020-03-20 15:46:22 +0800

    User类添加ErrorMessage

--- BLL/Entity/User.cs
+++ BLL/Entity/User.cs
@@ -10,13 +10,13 @@ namespace Entity
     public class User
     {
         public int Id { get; set; }
-        [Required]
+        [Required(ErrorMessage ="*用户名不能为空")]
         public string Name { get; set; }
-        [Required]
+        [Required(ErrorMessage = "*密码不能为空")]
         public string Password { get; set; }
-        [Required]
+        [Required(ErrorMessage = "*邀请人不能为空")]
         public int InviterId { get; set; }
-        [Required]
+        [Required(ErrorMessage = "*邀请码不能为空")]
         public int InviteCode { get; set; }
     }
 }
