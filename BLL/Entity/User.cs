304e85cac897506b0a3fd1b395e0f6f54d41e036
402290540@qq.com <402290540@qq.com>
2020-03-20 15:40:52 +0800

    给User类添加required constraint

--- BLL/Entity/User.cs
+++ BLL/Entity/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +10,14 @@ namespace Entity
     public class User
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string Password { get; set; }
+        [Required]
         public int InviterId { get; set; }
+        [Required]
         public int InviteCode { get; set; }
     }
 }
+                
---
+++ BLL/Repository/Migrations/202003200734077_Add_requied_constraint.cs
@@ -0,0 +1,20 @@
+﻿namespace Repository.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Add_requied_constraint : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Users", "Name", c => c.String(nullable: false));
+            AlterColumn("dbo.Users", "Password", c => c.String(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Users", "Password", c => c.String());
+            AlterColumn("dbo.Users", "Name", c => c.String());
+        }
+    }
+}
