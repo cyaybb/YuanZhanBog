4424139e20b0dbcdbe5159c8d6ee4c2d5ddcb633
402290540@qq.com <402290540@qq.com>
2020-03-20 15:12:26 +0800

    配置连接数据库到第一次 Enable-Migrations

---
+++ BLL/Repository/Migrations/202003200347142_User.cs
@@ -0,0 +1,29 @@
+﻿namespace Repository.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class User : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Users",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Password = c.String(),
+                        InviterId = c.Int(nullable: false),
+                        InviteCode = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Users");
+        }
+    }
+}
--- BLL/Repository/UserRepository.cs
+++ BLL/Repository/UserRepository.cs
@@ -5,13 +5,17 @@ using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration;
 
 namespace Repository
 {
     public class UserRepository : DbContext
     {
+        static readonly string connectionString = ConfigurationManager.ConnectionStrings["YuanZhan"].ConnectionString;
         public DbSet<User> Users { get; set; }
-
+        public UserRepository() : base("YuanZhan")
+        {
+        }
         public User Save()
         {
             return new User();
